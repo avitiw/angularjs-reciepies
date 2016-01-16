d9d8633a7e82146a9258dbb3afdcc0dd1538fff0
Avinash Tiwari <avitiw@gmail.com>
2016-01-16 22:48:16 +0530

    SelectNum Directive

--- public/js/app/modalController.js
+++ public/js/app/modalController.js
@@ -37,4 +37,33 @@
 
     ModalAgeController.$inject = ['$scope', '$log','$modalInstance'];
     recipiesApp.controller('ModalAgeController',ModalAgeController);
+
+    recipiesApp.directive('ngSelectNum', ['$parse', function ($parse) {
+         
+        var link = function (scope, element, attr, ctrls) {
+            var model = $parse(attr.angularModel) ;
+            var modelSet = model.assign; 
+            element.bind('change', function () {
+                scope.$apply(function () {
+                    var value = element.val();
+                    modelSet && modelSet(scope, value);
+                });
+            }); 
+           
+        };
+        return {
+            restrict: 'A', 
+            compile: function (elem, attrs) {
+                var innerHtml = "<option value='select'>select</option>";
+                for (i = attrs.min; i <= attrs.max; i++) {
+                    var item =  i;
+                    innerHtml += "<option value='" + item + "'>" + item +"</option>";
+                }
+                elem.html(innerHtml);
+
+                return link;
+            }
+           
+        };
+    }]);
 })()
